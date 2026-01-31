9c7d54de0229d06a855b1d45d293142c44041dad
Elite <elite@gmail.com>
2026-01-31 12:22:50 +0300

    وصف التعديل

--- api.go
+++ api.go
@@ -1,7 +1,39 @@
 package main
-type APIserver struct {
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type apiFunc func (http.ResponseWriter, *http.Request) error 
+	
+func makeHTTPHandlFunc(f apiFunc) http.HandlerFunc {
+type APIServer struct {
 	listenAddr string
 }
-func NewAPIServer(listenAddr string) *APIserver {
-	return &APIserver{listenAddr: listenAddr}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{listenAddr: listenAddr}
+}
+
+func (s *APIServer) Run(){
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", s.handleAccount)
+}
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handlegetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/777338538/gobank
 
 go 1.25.5
+
+require github.com/gorilla/mux v1.8.1
